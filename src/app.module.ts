ecea93f4eec17e159d6d9fd703a7ad02aa7ee1be
Region60 <_max_kot@mail.ru>
2022-02-24 14:04:28 +0300

    start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { CatsService } from './cats/cat.service';
+import { CatsController } from './cats/cats.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+  imports: [CatsModule],
+  })
+export class AppModule { }
