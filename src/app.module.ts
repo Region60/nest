18f6ddce1a7ae93428162e84ca60af3806bddedd
Region60 <_max_kot@mail.ru>
2022-02-25 18:09:47 +0300

    work 25-02

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { UsersModule } from './users/users.module';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [UsersModule, AuthModule],
+  providers: [AuthService],
   })
 export class AppModule { }
--- src/users/user.controller.ts
+++
@@ -1,2 +0,0 @@
-import { Controller, Get, Post } from "@nestjs/common";
-i
