ea4d9810315f82ef9d991923e699d4f7b901f2fe
Region60 <_max_kot@mail.ru>
2022-03-04 22:41:43 +0300

    __

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
-import { CatsService } from './cats/cat.service';
-import { CatsController } from './cats/cats.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
