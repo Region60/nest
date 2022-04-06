ded81216bb1a6b92a8cb3d6f33e98d2a3ba3d882
Region60 <_max_kot@mail.ru>
2022-04-07 01:40:29 +0300

    home--07-04-2022

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
+import helmet from 'helmet'
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   console.log("написать тесты")
+  console.log("захешировать пароль")
   console.log("реализовать два вида регистрации и два вида логинизации")
 
   const app = await NestFactory.create(AppModule);
+  app.use(helmet())
   app.useGlobalPipes(new ValidationPipe)
   await app.listen(3000);
 }
